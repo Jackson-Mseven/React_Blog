ac36324e913dfac9e327309e735a15aac6897f48
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-18 15:08:58 +0800

    ci: 👷 init
    init
    
    BREAKING CHANGE: 🧨 none

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    "react/prop-types": "off",
-    "no-unused-vars": "warn",
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    'plugin:react/jsx-runtime'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    "react": {
+      "version": "detect"
+    }
+  }
+};
