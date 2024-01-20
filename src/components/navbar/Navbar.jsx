0f03e77f00995020b675ab612059c3bf991c3d92
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-20 21:53:56 +0800

    feat: ✨ 完成侧边栏

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,20 @@
+import { motion } from 'framer-motion'
+
 import './navbar.scss'
+import Sidebar from '@/components/sidebar/Sidebar'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      {/* Sidebar */}
+      <Sidebar></Sidebar>
       <div className='wrapper'>
-        <span>Jackson Mseven</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Jackson Mseven
+        </motion.span>
         <div className='social'>
           <a href='#'>
             <img src='public/facebook.png' />
