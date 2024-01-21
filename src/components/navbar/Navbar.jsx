b09c9e034f1a66abe9c6ac368bd4791c78cf49ec
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-21 23:09:26 +0800

    style: 💄 修改首页 UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,3 @@
-import { motion } from 'framer-motion'
-
 import './navbar.scss'
 import Sidebar from '@/components/sidebar/Sidebar'
 
@@ -8,13 +6,6 @@ const Navbar = () => {
     <div className='navbar'>
       <Sidebar></Sidebar>
       <div className='wrapper'>
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
-        >
-          Jackson Mseven
-        </motion.span>
         <div className='social'>
           <a href='#'>
             <img src='public/facebook.png' />
