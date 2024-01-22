4e4835d6b918d15662e8d1e8b28afd4760c416a5
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-22 21:18:51 +0800

    feat: ✨ 完成视差页

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './app.scss'
 import Navbar from '@/components/navbar/Navbar'
 import Hero from '@/components/hero/Hero'
+import Parallax from '@/components/parallax/Parallax'
 
 const App = () => {
   return (
@@ -9,9 +10,13 @@ const App = () => {
         <Navbar></Navbar>
         <Hero></Hero>
       </section>
-      <section id='Services'>Parallax</section>
+      <section id='Services'>
+        <Parallax type='services'></Parallax>
+      </section>
       <section>Services</section>
-      <section id='Portfolio'>Parallax</section>
+      <section id='Portfolio'>
+        <Parallax type='portfolio'></Parallax>
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
