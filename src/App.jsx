39e28312845655065009e10431d398472892513b
Jackson-Mseven <112267658+Jackson-Mseven@users.noreply.github.com>
2024-01-23 21:37:53 +0800

    ci: 👷 新增git自动上传

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from '@/components/hero/Hero'
 import Parallax from '@/components/parallax/Parallax'
 import Services from '@/components/services/Services'
 import Portfolio from '@/components/portfolio/Portfolio'
+import Contact from '@/components/contact/Contact'
 
 const App = () => {
   return (
@@ -22,7 +23,9 @@ const App = () => {
         <Parallax type='portfolio'></Parallax>
       </section>
       <Portfolio></Portfolio>
-      <section id='Contact'>Contact</section>
+      <section id='Contact'>
+        <Contact></Contact>
+      </section>
     </div>
   )
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,18 @@
+import './contact.scss'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className='textContainer'>
+        <h1>{"Let's work together"}</h1>
+        <div className='item'>
+          <h2>Mail Me</h2>
+          <span>bill714@foxmail.com</span>
+        </div>
+      </div>
+      <div className='formContainer'></div>
+    </div>
+  )
+}
+
+export default Contact
